f9b36e7b37c1f71e23639ce831ac13d3d7494a87
Hans-Peter Locher <locher.hanspeter@gmail.com>
2008-02-18 08:59:02 +0000

    0.2 build 15: added emailview, default view for IEmail.

--- mailtoplone/base/TODO.rst
+++ mailtoplone/base/TODO.rst
@@ -5,4 +5,19 @@ mailtoplone base TODO
 :Date:      $Date$
 :Revision:  $Revision$
 
+emailview
+---------
+
+- decode headers::
+
+  from email.Header import decode_header
+  yield dict( name=name, content=[header for header in decode_header(m[name])])
+
+- body needs to be decoded also,
+  therefore need to determine encoding of body
+  change BodyFactory utility to extract the charset for the
+  returned body and return, body, content_type, charset
+
+- create download for attachments
+
  vim: set ft=rst tw=75 nocin nosi ai sw=4 ts=4 expandtab:
---
+++ mailtoplone/base/browser/emailview.py
@@ -0,0 +1,140 @@
+# -*- coding: utf-8 -*-
+#
+# File: emailview.py
+#
+# Copyright (c) InQuant GmbH
+#
+#  This program is free software; you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation; either version 2 of the License, or
+#  (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with this program; if not, write to the Free Software
+#  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+__author__    = """Hans-Peter Locher<hans-peter.locher@inquant.de>"""
+__docformat__ = 'plaintext'
+__revision__  = "$Revision: 36831 $"
+__version__   = '$Revision: 1.7 $'[11:-2]
+
+import email
+from email.Header import decode_header
+
+from zope.interface import implements, Interface
+from zope import component
+
+from Products.Five import BrowserView
+from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
+
+from plone.memoize.instance import memoize
+
+from mailtoplone.base import baseMessageFactory as _
+from mailtoplone.base.interfaces import IEmail
+from mailtoplone.base.interfaces import IBodyFactory
+
+class IEmailView(Interface):
+    """
+      EmailView interface
+    """
+    
+    def test():
+        """ test method"""
+    
+
+            
+class EmailView(BrowserView):
+    """
+    EmailView for having a nice representation of the Email
+    """
+    implements(IEmailView)
+        
+    def __init__(self, context, request):
+        # do stuff which is always needed here (convert data to email object e.g.)
+        self.context = context
+        self.request = request
+
+    render = ViewPageTemplateFile('emailview.pt')
+
+    def __call__(self):
+        if self.request.get("download"):
+            pass
+            # do download here
+        else:
+            return self.render()
+
+    @property
+    def title(self):
+        return self.context.title
+    
+    @memoize
+    def headers(self):
+        headerlist = ['Subject','From','To','CC']
+        m = email.message_from_string(self.context.data)
+        for name in headerlist:
+            if m.has_key(name):
+                header = m[name]
+                yield dict( name=name, content=header)
+
+
+    def getAttachmentById(self, id):
+        """ this results in a file-like object and mimetype """
+        return file("/tmp/muha.txt"), "text/plain"
+
+
+    def attachments(self):
+        """return a generator which yields dicts like
+            { "mimetype": "text/plain",
+              "id":       "someidwhichisunique",
+              "filename": "muha.txt" }
+        """
+        pass
+        #yield dict(
+        #        mimetype='text/plain',
+        #        filename='filexy.txt',
+        #        id='boundaryidosonstiggeid',
+        #        download_url="http://www.web.de"
+        #        )
+        #for part in email.walk():
+        #    if part.is_attachment():
+        #        boundary_id = part.getid()
+        #        # use urlquote here (urllib.urlquote)
+        #        url="%s/view?download=%s&mimetype=%s&filename=%s" % (
+        #                self.context.absolute_url(),
+        #                boundary_id,
+        #                mimetype,
+        #                fn )
+
+        #        yield dict(
+        #                mimetype=mimetype,
+        #                filename=fn,
+        #                id=boundary_id,
+        #                download_url=url
+        #                )
+
+
+    @memoize
+    def body(self):
+        """this is bad because of
+        Insertion of non-unicode non-ascii non-utf8 encoded text in TAL is deprecated and will be broken in Plone 3.5 !!!
+        """
+        bodyfactory = component.getUtility(IBodyFactory)
+        body, content_type = bodyfactory(self.context.data)
+        # xxx we need to encode the body appropriate, therefore the utility needs to return the encoding of the part
+        return body
+
+    
+    def test(self):
+        """ 
+        test method 
+        """
+        dummy = _(u'a dummy string')
+        
+        return {'dummy': dummy}
+
+# vim: set ft=python ts=4 sw=4 expandtab :
