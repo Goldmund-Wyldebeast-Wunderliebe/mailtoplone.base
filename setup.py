42f2b1504c96271ade98c6d82f9afb898bd16805
Hans-Peter Locher <locher.hanspeter@gmail.com>
2008-12-02 10:47:06 +0000

    update setup.py (more documentation, fix version number to 0.2.6 ( 0.2.5 is the last released version)

--- setup.py
+++ setup.py
@@ -1,11 +1,41 @@
+# -*- coding: utf-8 -*-
+"""
+This module contains the tool of mailtoplone.base
+"""
+import os
 from setuptools import setup, find_packages
 
-version = '0.2.7'
+def read(*rnames):
+    return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
+
+version = '0.2.6'
+
+long_description = (
+    read('README.txt')
+    + '\n' +
+    'Change history\n'
+    '**************\n'
+    + '\n' +
+    read('CHANGES.txt')
+    + '\n' +
+    'Detailed Documentation\n'
+    '**********************\n'
+    + '\n' +
+    read('mailtoplone', 'base', 'README.txt')
+    + '\n' +
+    'Contributors\n'
+    '************\n'
+    + '\n' +
+    read('CONTRIBUTORS.txt')
+    + '\n' +
+    'Download\n'
+    '********\n'
+    )
 
 setup(name='mailtoplone.base',
       version=version,
       description="basic package for mailtoplone",
-      long_description=file("README.txt").read(),
+      long_description=long_description,
       # Get more strings from http://www.python.org/pypi?%3Aaction=list_classifiers
       classifiers=[
         "Framework :: Plone",
